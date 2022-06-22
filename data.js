a875e1e169be42b54e5f358c06eab69e2dbbac47
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2022-06-22 20:38:54 +0530

    Files added

---
+++ data.js
@@ -0,0 +1,31 @@
+let movies = [
+    {
+        name:'falcon and winter soldier',
+        des:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt inventore quaerat saepe vel consequuntur nostrum a doloremque excepturi earum, sit veniam consectetur tenetur facilis sint, qui architecto error porro fuga quod vero. Quasi molestias commodi perspiciatis iste placeat ex cupiditate',
+        image: 'images/slider 2.PNG'
+    },
+
+    {
+        name:'loki',
+        des:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt inventore quaerat saepe vel consequuntur nostrum a doloremque excepturi earum, sit veniam consectetur tenetur facilis sint, qui architecto error porro fuga quod vero. Quasi molestias commodi perspiciatis iste placeat ex cupiditate',
+        image:'images/slider 1.PNG'
+    },
+
+    {
+        name:'wanda vision',
+        des:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt inventore quaerat saepe vel consequuntur nostrum a doloremque excepturi earum, sit veniam consectetur tenetur facilis sint, qui architecto error porro fuga quod vero. Quasi molestias commodi perspiciatis iste placeat ex cupiditate',
+        image:'images/slider 3.PNG'
+    },
+
+    {
+        name:'raya and the last dragon',
+        des:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt inventore quaerat saepe vel consequuntur nostrum a doloremque excepturi earum, sit veniam consectetur tenetur facilis sint, qui architecto error porro fuga quod vero. Quasi molestias commodi perspiciatis iste placeat ex cupiditate',
+        image:'images/slider 4.PNG'
+    },
+
+    {
+        name:'luca',
+        des:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt inventore quaerat saepe vel consequuntur nostrum a doloremque excepturi earum, sit veniam consectetur tenetur facilis sint, qui architecto error porro fuga quod vero. Quasi molestias commodi perspiciatis iste placeat ex cupiditate',
+        image:'images/slider 5.PNG'
+    }
+]
